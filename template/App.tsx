381b60cc1e877cca500695bd97a71a7cf49b8280
Andrey <ramirez.jag00@gmail.com>
2023-09-02 04:50:03 +0800

    fix: podfile.lock and RN version

--- template/App.tsx
+++ template/App.tsx
@@ -1,11 +1,14 @@
-import { COLORS, CONTAINERS } from './src/constants/themes'
 import { SafeAreaView, StyleSheet, Text } from 'react-native'
+
+import { COLORS, CONTAINERS } from './src/constants/themes'
+
+import packageJSON from './package.json'
 
 const App: React.FC = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>
-        React Native Template Typescript - RN v0.71.10
+        React Native Template Typescript - RN {packageJSON.dependencies['react-native']}
       </Text>
     </SafeAreaView>
   )
